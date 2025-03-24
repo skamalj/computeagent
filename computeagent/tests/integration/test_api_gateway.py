694a3832e8ec319c74fce35c5aa3b92285bdbea0
Kamal <dummy@outlook.com>
2025-03-24 16:50:03 +0530

    test file updated

--- computeagent/tests/integration/test_api_gateway.py
+++ computeagent/tests/integration/test_api_gateway.py
@@ -1,45 +1,74 @@
-import os
-
-import boto3
-import pytest
+# @! create function to call aws api gateway with post request using api key 
+#  
 import requests
+import os
+from langchain_core.messages import utils
 
-"""
-Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
-"""
+def create_tools_json(tools):
+    tools_json = []
+    for tool in tools:
+        tool_info = {
+            'name': tool.name,
+            'description': tool.description,
+            'args_schema': tool.args_schema.model_json_schema()
+        }
+        tools_json.append(tool_info)
+    return tools_json
+
+data = {
+    "provider": "openai",    
+    "model_name": "gpt-4o",  
+    "params": {              
+        "temperature": 0.7,
+        "max_tokens": 100
+    },
+     "messages": [{"content": "talk with me about sports", "role": "human"}]
+}
+
+api_key = os.environ.get('API_GW_KEY')
+api_url = os.environ.get('API_GW_URL')
+def call_api_gateway(api_url, api_key, data):
+    headers = {
+        'x-api-key': api_key,
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(api_url, headers=headers, json=data)
+    return response.json()
 
 
-class TestApiGateway:
 
-    @pytest.fixture()
-    def api_gateway_url(self):
-        """ Get the API Gateway URL from Cloudformation Stack outputs """
-        stack_name = os.environ.get("AWS_SAM_STACK_NAME")
+from langchain_core.messages import AIMessage
+from langchain_core.tools import tool
 
-        if stack_name is None:
-            raise ValueError('Please set the AWS_SAM_STACK_NAME environment variable to the name of your stack')
+from langgraph.prebuilt import ToolNode
 
-        client = boto3.client("cloudformation")
+@tool
+def get_weather(location: str):
+    """Call to get the current weather."""
+    if location.lower() in ["sf", "san francisco"]:
+        return "It's 60 degrees and foggy."
+    else:
+        return "It's 90 degrees and sunny."
 
-        try:
-            response = client.describe_stacks(StackName=stack_name)
-        except Exception as e:
-            raise Exception(
-                f"Cannot find stack {stack_name} \n" f'Please make sure a stack with the name "{stack_name}" exists'
-            ) from e
 
-        stacks = response["Stacks"]
-        stack_outputs = stacks[0]["Outputs"]
-        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "HelloWorldApi"]
+@tool
+def get_coolest_cities():
+    """Get a list of coolest cities"""
+    return "nyc, sf"
 
-        if not api_outputs:
-            raise KeyError(f"HelloWorldAPI not found in stack {stack_name}")
+from langgraph_utils import json_to_structured_tools
+import json
+tools = [get_weather, get_coolest_cities]
+tool_node = ToolNode(tools)
+tools_json = create_tools_json(tools)
 
-        return api_outputs[0]["OutputValue"]  # Extract url from stack outputs
+data["tools"] = tools_json
+data["messages"] = utils.convert_to_openai_messages([AIMessage(content="What's the weather in SF?", role="human")])
+print(tools_json)
+print(json_to_structured_tools(tools_json))
 
-    def test_api_gateway(self, api_gateway_url):
-        """ Call the API Gateway endpoint and check the response """
-        response = requests.get(api_gateway_url)
+response = call_api_gateway(api_url, api_key, data)
+print(response)
+ai_message = AIMessage(**response)
 
-        assert response.status_code == 200
-        assert response.json() == {"message": "hello world"}
+tool_node.invoke({"messages": [ai_message]})
