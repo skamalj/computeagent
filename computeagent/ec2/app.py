d6ede1893a548c6ecc3c3ea92073c1265dc947de
Kamal <dummy@outlook.com>
2025-03-26 11:05:37 +0530

    fix

--- computeagent/ec2/app.py
+++ computeagent/ec2/app.py
@@ -94,7 +94,7 @@ def delete_messages(state: MessagesState, n=int(os.getenv("MSG_HISTORY_TO_KEEP")
             return {"messages": []}  # No deletion needed
         else:
             print(f"Total messages: {len(messages)}, deleting all except last {n} messages.")
-            return {"messages": [RemoveMessage(id=m.id) for m in messages[1:delete_trigger_count-n]]} 
+            return {"messages": [RemoveMessage(id=m.id) for m in messages[n:]]} 
 
         messages_to_keep = messages[-n:]  # Keep last `n` messages
         messages_to_remove = messages[:-n]  # Messages to be removed
