b3c990c795f19fc4acace5cf133b8c637cba10d5
Kamal <dummy@outlook.com>
2025-03-27 14:55:03 +0530

    fix

--- computeagent/ec2/app.py
+++ computeagent/ec2/app.py
@@ -146,9 +146,6 @@ def lambda_handler(event, context):
         message = extract_whatsapp_messages(body)
         recipeint = extract_recipient(body)
         config = {"configurable": {"thread_id": recipeint}}
-        messages = app.get_state(config).values.get("messages", [])
-        if len(messages) > 10:
-            app.update_state(config, {"messages": [RemoveMessage(id=m.id) for m in messages[1:6]]})
         print(f"Message recieved from {recipeint}:  {message}")
         input_message = {
             "messages": [
